ec1e679bb21bba0b39aaccb27d0cfaf76bc4c8c9
Kovshik387 <fantokin03@mail.ru>
2025-07-11 03:40:51 +0300

    fix: add tests

--- InternshipTask/tests/InternshipTask.IntegrationTests/RepositoryTests/GameRepositoryTests.cs
+++ InternshipTask/tests/InternshipTask.IntegrationTests/RepositoryTests/GameRepositoryTests.cs
@@ -1,82 +1,82 @@
-﻿// using FluentAssertions;
-// using InternshipTask.Domain.Models.Enums;
-// using InternshipTask.Domain.Repositories;
-// using InternshipTask.Infrastructure.Repositories.Exceptions;
-// using InternshipTask.IntegrationTests.Fakers;
-// using InternshipTask.IntegrationTests.Fixtures;
-// using Xunit;
-//
-// namespace InternshipTask.IntegrationTests.RepositoryTests;
-//
-// [Collection(nameof(TestFixture))]
-// public class GameRepositoryTests
-// {
-//     private readonly IGameRepository _gameRepository;
-//
-//     public GameRepositoryTests(TestFixture fixture)
-//     {
-//         _gameRepository = fixture.GameRepository;
-//     }
-//
-//     [Fact]
-//     public async Task CreateGame_Should_ReturnIdGame()
-//     {
-//         // Arrange
-//         var game = GameModelFaker.Generate();
-//         
-//         // Act
-//         var result = await _gameRepository.CreateGameAsync(game);
-//         
-//         // Assert
-//         result.Should().Be(game.Id);
-//     }
-//     
-//     [Fact]
-//     public async Task GetGameByIdAsync_Should_ReturnGame()
-//     {
-//         // Arrange
-//         var game = GameModelFaker.Generate();
-//         await _gameRepository.CreateGameAsync(game);
-//
-//         // Act
-//         var result = await _gameRepository.GetGameByIdAsync(game.Id);
-//
-//         // Assert
-//         result.Should().NotBeNull();
-//         result.Should().BeEquivalentTo(game, x =>
-//             x.Excluding(f => f.GameFinishedAt));
-//     }
-//     
-//     [Fact]
-//     public async Task UpdateGameAsync_Should_UpdateGameSuccessfully()
-//     {
-//         // Arrange
-//         var game = GameModelFaker.Generate();
-//         await _gameRepository.CreateGameAsync(game);
-//         var updateGame = game.
-//             WithStatus(GameStatus.Win).
-//             WithTimeFinished(DateTime.UtcNow);
-//         
-//         // Act
-//         await _gameRepository.UpdateGameAsync(updateGame);
-//
-//         // Assert
-//         var result = await _gameRepository.GetGameByIdAsync(game.Id);
-//         result!.Status.Should().Be(GameStatus.Win);
-//         result.GameFinishedAt.Should().NotBeNull();
-//     }
-//     
-//     
-//     [Fact]
-//     public async Task UpdateGameAsync_ShouldThrow_WhenGameNotExists()
-//     {
-//         // Arrange
-//         var game = GameModelFaker.Generate();
-//
-//         // Act
-//         var act = async () => await _gameRepository.UpdateGameAsync(game);
-//
-//         // Assert
-//         await act.Should().ThrowAsync<FailedGameUpdateException>();
-//     }
-// }
+﻿using FluentAssertions;
+using InternshipTask.Domain.Models.Enums;
+using InternshipTask.Domain.Repositories;
+using InternshipTask.Infrastructure.Repositories.Exceptions;
+using InternshipTask.IntegrationTests.Fakers;
+using InternshipTask.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace InternshipTask.IntegrationTests.RepositoryTests;
+
+[Collection(nameof(TestFixture))]
+public class GameRepositoryTests
+{
+    private readonly IGameRepository _gameRepository;
+
+    public GameRepositoryTests(TestFixture fixture)
+    {
+        _gameRepository = fixture.GameRepository;
+    }
+
+    [Fact]
+    public async Task CreateGame_Should_ReturnIdGame()
+    {
+        // Arrange
+        var game = GameModelFaker.Generate();
+        
+        // Act
+        var result = await _gameRepository.CreateGameAsync(game);
+        
+        // Assert
+        result.Should().Be(game.Id);
+    }
+    
+    [Fact]
+    public async Task GetGameByIdAsync_Should_ReturnGame()
+    {
+        // Arrange
+        var game = GameModelFaker.Generate();
+        await _gameRepository.CreateGameAsync(game);
+
+        // Act
+        var result = await _gameRepository.GetGameByIdAsync(game.Id);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(game, x =>
+            x.Excluding(f => f.GameFinishedAt));
+    }
+    
+    [Fact]
+    public async Task UpdateGameAsync_Should_UpdateGameSuccessfully()
+    {
+        // Arrange
+        var game = GameModelFaker.Generate();
+        await _gameRepository.CreateGameAsync(game);
+        var updateGame = game.
+            WithStatus(GameStatus.Win).
+            WithTimeFinished(DateTime.UtcNow);
+        
+        // Act
+        await _gameRepository.UpdateGameAsync(updateGame);
+
+        // Assert
+        var result = await _gameRepository.GetGameByIdAsync(game.Id);
+        result!.Status.Should().Be(GameStatus.Win);
+        result.GameFinishedAt.Should().NotBeNull();
+    }
+    
+    
+    [Fact]
+    public async Task UpdateGameAsync_ShouldThrow_WhenGameNotExists()
+    {
+        // Arrange
+        var game = GameModelFaker.Generate();
+
+        // Act
+        var act = async () => await _gameRepository.UpdateGameAsync(game);
+
+        // Assert
+        await act.Should().ThrowAsync<FailedGameUpdateException>();
+    }
+}
--- InternshipTask/tests/InternshipTask.IntegrationTests/RepositoryTests/MoveRepositoryTests.cs
+++ InternshipTask/tests/InternshipTask.IntegrationTests/RepositoryTests/MoveRepositoryTests.cs
@@ -1,65 +1,65 @@
-﻿// using FluentAssertions;
-// using InternshipTask.Domain.Repositories;
-// using InternshipTask.Infrastructure.Repositories.Exceptions;
-// using InternshipTask.IntegrationTests.Fakers;
-// using InternshipTask.IntegrationTests.Fixtures;
-// using Xunit;
-//
-// namespace InternshipTask.IntegrationTests.RepositoryTests;
-//
-// [Collection(nameof(TestFixture))]
-// public class MoveRepositoryTests
-// {
-//     private readonly IMoveRepository _moveRepository;
-//
-//     public MoveRepositoryTests(TestFixture fixture)
-//     {
-//         _moveRepository = fixture.MoveRepository;
-//     }
-//
-//     [Fact]
-//     public async Task MakeMoveAsync_Should_ReturnSuccess()
-//     {
-//         // Arrange
-//         var move = MoveModelFaker.Generate().First();
-//         
-//         // Act
-//         var act = async () => await _moveRepository.MakeMoveAsync(move);
-//
-//         // Assert
-//         await act.Should().NotThrowAsync<FailedMoveAddException>();
-//     }
-//  
-//     [Fact]
-//     public async Task GetMovesByGameIdAsync_ShouldReturnAllMovesOrderedByCount()
-//     {
-//         // Arrange
-//         var gameId = Guid.NewGuid();
-//         var moves = MoveModelFaker.Generate(3, gameId);
-//         foreach (var move in moves)
-//             await _moveRepository.MakeMoveAsync(move);
-//
-//         // Act
-//         var result = await _moveRepository.GetMovesByGameIdAsync(gameId);
-//
-//         // Assert
-//         result.Should().HaveCount(3);
-//         result.Should().BeInAscendingOrder(x => x.MoveCount);
-//     }
-//  
-//     [Fact]
-//     public async Task GetMoveByETagAsync_ShouldReturnCorrectMove()
-//     {
-//         // Arrange
-//         var move = MoveModelFaker.Generate().First();
-//         await _moveRepository.MakeMoveAsync(move);
-//
-//         // Act
-//         var result = await _moveRepository.GetMoveByETagAsync(move.GameId, move.ETag);
-//
-//         // Assert
-//         result.Should().NotBeNull();
-//         result.Should().BeEquivalentTo(move);
-//     }
-//     
-// }
+﻿using FluentAssertions;
+using InternshipTask.Domain.Repositories;
+using InternshipTask.Infrastructure.Repositories.Exceptions;
+using InternshipTask.IntegrationTests.Fakers;
+using InternshipTask.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace InternshipTask.IntegrationTests.RepositoryTests;
+
+[Collection(nameof(TestFixture))]
+public class MoveRepositoryTests
+{
+    private readonly IMoveRepository _moveRepository;
+
+    public MoveRepositoryTests(TestFixture fixture)
+    {
+        _moveRepository = fixture.MoveRepository;
+    }
+
+    [Fact]
+    public async Task MakeMoveAsync_Should_ReturnSuccess()
+    {
+        // Arrange
+        var move = MoveModelFaker.Generate().First();
+        
+        // Act
+        var act = async () => await _moveRepository.MakeMoveAsync(move);
+
+        // Assert
+        await act.Should().NotThrowAsync<FailedMoveAddException>();
+    }
+ 
+    [Fact]
+    public async Task GetMovesByGameIdAsync_ShouldReturnAllMovesOrderedByCount()
+    {
+        // Arrange
+        var gameId = Guid.NewGuid();
+        var moves = MoveModelFaker.Generate(3, gameId);
+        foreach (var move in moves)
+            await _moveRepository.MakeMoveAsync(move);
+
+        // Act
+        var result = await _moveRepository.GetMovesByGameIdAsync(gameId);
+
+        // Assert
+        result.Should().HaveCount(3);
+        result.Should().BeInAscendingOrder(x => x.MoveCount);
+    }
+ 
+    [Fact]
+    public async Task GetMoveByETagAsync_ShouldReturnCorrectMove()
+    {
+        // Arrange
+        var move = MoveModelFaker.Generate().First();
+        await _moveRepository.MakeMoveAsync(move);
+
+        // Act
+        var result = await _moveRepository.GetMoveByETagAsync(move.GameId, move.ETag);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(move);
+    }
+    
+}
